7b88f5de69a847ba826609fa9575e49006100604
Lindsey Lawson <lynseahoss@gmail.com>
2020-07-28 15:31:23 -0400

    resized card and added mainPigeon.png for test purposes of card

--- app/components/Card/Card.js
+++ app/components/Card/Card.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
 
-import AppText from "../AppText/AppText"
+import AppText from "../AppText/AppText";
 import colors from "../../config/colors";
 
-
-
-function Card ({title, subTitle, image}) {
+function Card({ title, subTitle, image }) {
   return (
     <View style={styles.card}>
-     <Image source={image}/>
-     <AppText>{title}</AppText>
-     <AppText>{subTitle}</AppText>
+      <Image style={styles.image} source={image} />
+      <AppText>{title}</AppText>
+      <AppText>{subTitle}</AppText>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  card:{
-    borderRadius:15,
-    backgroundColor:colors.pink,
-    marginBottom:20,
+  card: {
+    borderRadius: 15,
+    backgroundColor: colors.pink,
+    marginBottom: 20,
+    marginTop: 100,
+
+    justifyContent: "center",
+    alignItems: "center"
   },
-  
+  image: {
+    width: "100%",
+    height: 100,
+    resizeMode: "contain"
+  }
 });
 export default Card;
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,29 +1,27 @@
-
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
-import Card from "../components/Card/Card"
+import Card from "../components/Card/Card";
 
 import colors from "../config/colors";
 
-
-
-function ViewImageScreen ({title, subTitle, image}) {
+function ViewImageScreen({ title, subTitle, image }) {
   return (
     <View style={styles.container}>
-      <Card title="Sprinkles" subTitle="I can eat numerous sprinkle donuts daily." image={require("../assets/mainPigeon.png")}/>
+      <Card
+        title="Sprinkles"
+        subTitle="I can eat numerous sprinkle donuts daily."
+        image={require("../assets/mainPigeon.png")}
+      />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  card:{
-    borderRadius:15,
-    backgroundColor:colors.pink,
-    marginBottom:20,
-  },
   container: {
     backgroundColor: colors.white,
-    flex: 1
-  },
+    flex: 1,
+    padding: 29,
+    paddingTop: 100
+  }
 });
 export default ViewImageScreen;
